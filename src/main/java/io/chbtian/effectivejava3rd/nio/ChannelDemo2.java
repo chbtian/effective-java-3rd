1b243cc34fd41d8b6deef49d4d28b9b07acae6c3
tcb <chbtian@163.com>
2020-12-09 17:32:07 +0800

    blocking io

---
+++ src/main/java/io/chbtian/effectivejava3rd/nio/ChannelDemo2.java
@@ -0,0 +1,127 @@
+package io.chbtian.effectivejava3rd.nio;
+
+import com.sun.beans.editors.ByteEditor;
+import jdk.management.resource.internal.inst.FileChannelImplRMHooks;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+/**
+ *  --实现多路复用和非阻塞IO
+ *  SelectableChannel
+ *      --SocketChannel
+ *      --ServerSocketChannel
+ *      --DatagramChannel
+ *   Selector
+ *   SelectionKey
+ */
+public class ChannelDemo2 {
+
+    @Test
+    public void bCliet() throws IOException {
+        SocketChannel sc = SocketChannel.open(new InetSocketAddress("127.0.0.1",8899));
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            buf.put(scanner.next().getBytes());
+            buf.flip();
+            sc.write(buf);
+            buf.clear();
+
+            sc.read(buf);
+            System.out.println(new String(buf.array()));
+            buf.clear();
+        }
+    }
+
+    @Test
+    public void bServer() throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress("127.0.0.1",8899));
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        SocketChannel sc = ssc.accept();
+        while(true) {
+            sc.read(buf);
+            buf.flip();
+            System.out.println(new String(buf.array()));//使用底层数组是不对的
+            buf.clear();
+            buf.put("收到".getBytes());
+            buf.flip();
+            sc.write(buf);
+            buf.clear();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Test
+    public void testBIOClient() throws IOException {
+
+       // Socket socket = new Socket("127.0.0.1",8898);
+        SocketChannel sc = SocketChannel.open(new InetSocketAddress("127.0.0.1",8898));
+        /**
+         * 注意：
+         * SocketChannel只有通过SocketChannel.open()才能创建
+         * 只使用Socket.getChannel()方法无法创建，会出现空指针
+         *
+         * ServerSocketChannel也是一样，只能通过open方法创建
+         * 创建后才可以使用getChannel()获取
+         *
+         * FileChannel可以通过文件流直接getChannel()获取
+         */
+       // SocketChannel sc = socket.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        FileChannel fc = FileChannel.open(Paths.get("a.txt"), StandardOpenOption.READ);
+        while(fc.read(buffer)!=-1){
+            buffer.flip();
+            sc.write(buffer);
+            buffer.clear();
+        }
+
+        sc.close();
+        fc.close();
+
+    }
+
+    @Test
+    public void testBIOServer() throws IOException {
+       // ServerSocket serverSocket = new ServerSocket(8898);
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress("127.0.0.1",8898));
+       // ServerSocket serverSocket = ssc.socket();
+       // System.out.println(serverSocket.getChannel() == ssc);
+       //ServerSocketChannel ssc = serverSocket.getChannel();
+        FileChannel fc = FileChannel.open(Paths.get("aaa.txt"),StandardOpenOption.CREATE,StandardOpenOption.WRITE);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        SocketChannel sc = ssc.accept();//这一步是阻塞的
+        while(sc.read(buffer)!=-1){
+            buffer.flip();
+            fc.write(buffer);
+            buffer.clear();
+        }
+        fc.close();
+        sc.close();
+        ssc.close();
+    }
+
+}
