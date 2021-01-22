c8247aa71a00a064049352d67fef4777c34d9486
tcb <chbtian@163.com>
2021-01-22 14:41:50 +0800

    cookie

--- src/main/java/io/chbtian/effectivejava3rd/controller/TestController.java
+++ src/main/java/io/chbtian/effectivejava3rd/controller/TestController.java
@@ -5,10 +5,23 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+
 @RestController
 public class TestController {
-    @PostMapping("/test")
-    public String test(String name) {
+    @GetMapping("/test")
+    public String test(String name, HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies!= null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }else{
+            response.addCookie(new Cookie("cid", LocalDateTime.now().toString()));
+        }
         return "hello " + name;
     }
 }
